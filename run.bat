63d2a504d7b9acb406e85c7fe65a0678bf5595dd
Angelo Gama <angelojbgama@gmail.com>
2024-11-04 15:51:05 -0300

    Retrutura a parte de pins do mapa para cada categoria do obj resultado. refatora a pagina home para estar centralizada.

--- run.bat
+++ run.bat
@@ -6,7 +6,8 @@ python manage.py makemigrations
 REM Efetuando as migrações
 python manage.py migrate
 
-REM Iniciando o servidor de desenvolvimento
-python manage.py runserver
+REM Iniciando o servidor de desenvolvimento em uma nova janela do shell
+start cmd /c "python manage.py runserver"
+
 
 pause
