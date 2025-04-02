4fbb51ab4cb23279fe5fff7e7fc8e2b1d724da02
Angelo <angelojbgama@gmail.com>
2025-04-01 23:18:27 -0300

    Adicionar rotas do aplicativo 'social' ao arquivo de URLs

--- bike2bike/urls.py
+++ bike2bike/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from social import views as social_views  # importa as views do app 'social'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls'), name='accounts'),
+    
+    path('', include('social.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
