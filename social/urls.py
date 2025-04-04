4bd7d7bf715e536fa6fba04a1663ac1eb2cfcaee
Angelo <angelojbgama@gmail.com>
2025-04-03 23:07:47 -0300

    Adicionar rotas para criação de diferentes tipos de postagens: normal, carrossel, enquete, evento e trajeto de bike

--- social/urls.py
+++ social/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import (
     feed_view,
-    PostCreateView,
+    PostTypeSelectView,
     PostDetailView,
+    NormalPostCreateView,
+    CarouselPostCreateView,
+    PollPostCreateView,
+    EventPostCreateView,
+    BikeRoutePostCreateView,
+    
 )
 from . import views  # Importa views.py para usar feed_more_view como FBV
 
@@ -11,7 +17,12 @@ urlpatterns = [
 
     path('feed/more/', views.feed_more_view, name='feed_more'),
 
-    path('nova-postagem/', PostCreateView.as_view(), name='nova_postagem'),
+    path('nova-postagem/', PostTypeSelectView.as_view(), name='nova_postagem_select'),
+    path('nova-postagem/normal/', NormalPostCreateView.as_view(), name='nova_postagem_normal'),
+    path('nova-postagem/carousel/', CarouselPostCreateView.as_view(), name='nova_postagem_carousel'),
+    path('nova-postagem/poll/', PollPostCreateView.as_view(), name='nova_postagem_poll'),
+    path('nova-postagem/event/', EventPostCreateView.as_view(), name='nova_postagem_event'),
+    path('nova-postagem/bikeroute/', BikeRoutePostCreateView.as_view(), name='nova_postagem_bikeroute'),
 
     path('publicacao/<int:pk>/', PostDetailView.as_view(), name='publicacao_detalhe'),
 ]
