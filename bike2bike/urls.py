e9e174e5efbfae6037a7828a07386bc326150043
Angelo Gama <angelojbgama@gmail.com>
2025-04-08 12:30:10 -0300

    Adicionar namespace à inclusão das URLs sociais

--- bike2bike/urls.py
+++ bike2bike/urls.py
@@ -7,7 +7,7 @@ from bike2bike.view import HomepageView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls'), name='accounts'),
-    path('social/', include('social.urls')),
+    path('social/', include('social.urls', namespace='social')),
     path('', HomepageView.as_view(), name='homepage'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
