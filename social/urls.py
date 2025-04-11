96e35601e4e574563eef4eec164a65684ae56d44
Angelo Gama <angelojbgama@gmail.com>
2025-04-11 10:27:27 -0300

    Organizar as rotas de postagem e adicionar URL para curtir posts

--- social/urls.py
+++ social/urls.py
@@ -1,22 +1,19 @@
 from django.urls import path
-from .views import *
 from . import views
 
 app_name = 'social'
 
 urlpatterns = [
     path('feed/', views.feed_view, name='feed'),
-
     path('feed/more/', views.feed_more_view, name='feed_more'),
-
-    path('nova-postagem/', PostTypeSelectView.as_view(), name='nova_postagem_select'),
-    path('nova-postagem/normal/', NormalPostCreateView.as_view(), name='nova_postagem_normal'),
-    path('nova-postagem/carousel/', CarouselPostCreateView.as_view(), name='nova_postagem_carousel'),
-    path('nova-postagem/poll/', PollPostCreateView.as_view(), name='nova_postagem_poll'),
-    path('nova-postagem/event/', EventPostCreateView.as_view(), name='nova_postagem_event'),
-    path('nova-postagem/bikeroute/', BikeRoutePostCreateView.as_view(), name='nova_postagem_bikeroute'),
-    
+    path('nova-postagem/', views.PostTypeSelectView.as_view(), name='nova_postagem_select'),
+    path('nova-postagem/normal/', views.NormalPostCreateView.as_view(), name='nova_postagem_normal'),
+    path('nova-postagem/carousel/', views.CarouselPostCreateView.as_view(), name='nova_postagem_carousel'),
+    path('nova-postagem/poll/', views.PollPostCreateView.as_view(), name='nova_postagem_poll'),
+    path('nova-postagem/event/', views.EventPostCreateView.as_view(), name='nova_postagem_event'),
+    path('nova-postagem/bikeroute/', views.BikeRoutePostCreateView.as_view(), name='nova_postagem_bikeroute'),
     path('upload-image/', views.upload_image, name='upload_image'),
-
-    path('publicacao/<int:pk>/', PostDetailView.as_view(), name='publicacao_detalhe'),
+    # URL para processar a ação de curtir
+    path('like_post/<int:post_id>/', views.like_post, name='like_post'),
+    path('publicacao/<int:pk>/', views.PostDetailView.as_view(), name='publicacao_detalhe'),
 ]
